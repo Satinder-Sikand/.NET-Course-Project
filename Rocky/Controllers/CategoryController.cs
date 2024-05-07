60850b774d63a27e11a19a8090040c95df6d3cfb
Satinder S <46537299+Satinder-Sikand@users.noreply.github.com>
2024-05-07 01:59:19 -0400

    Add project files.

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Security.Principal;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        //GET request for Create
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+
+        //POST request for Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+		//GET request for Edit
+		public IActionResult Edit(int? id)
+		{
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj==null) {
+                return NotFound();
+            }
+
+            return View(obj);
+		}
+
+		//POST request for Edit
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Category.Update(category);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(category);
+		}
+
+		//GET request for Delete
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var obj = _db.Category.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			return View(obj);
+		}
+
+		//POST request for Delete
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePost(int? id)
+		{
+			var obj = _db.Category.Find(id);
+			if (obj == null) { return NotFound();}
+			_db.Category.Remove(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        //To add to database, go to db context and add there. Then
+        //open package manager console and do 'add-migration Add___ToDatabase
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range( 1, int.MaxValue, ErrorMessage ="Display order for category must be greater than 0" )]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
